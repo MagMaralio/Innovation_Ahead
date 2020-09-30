7ea8af6f0dfca4109363cb25e708ee8cf6d4f88f
Aaromal Girish <aaromal303@gmail.com>
2020-09-30 22:52:31 +1000

    Add project files.

---
+++ Innovation_Ahead/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Innovation_Ahead.Controllers
+{
+    public class HomeController : Controller
+    {
+        VehiclesEntities context = new VehiclesEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Customer()
+        {
+            var query = from c in context.CARS
+                        select c;
+            return View(query.ToList());
+        }
+    }
+}
---
+++ Innovation_Ahead/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Innovation_Ahead.MvcApplication" Language="C#" %>
